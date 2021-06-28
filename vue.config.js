42a351d8500f44712c77fdb551b13c8743b8022d
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-06-28 17:27:24 +0900

    Add : material icon을 패키지에 포함

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
 module.exports = {
   devServer: {
     overlay: false
   },
-
-  transpileDependencies: ['vuetify']
+  lintOnSave: false,
+  css: {
+    extract: false
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        icons: path.join(__dirname, 'node_modules/vue-material-design-icons')
+      },
+      extensions: ['.vue']
+    }
+  }
 };
