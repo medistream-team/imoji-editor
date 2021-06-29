e7992194093adab162c6cf129338b86523f9db5b
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-06-29 21:34:20 +0900

    Add : navigation 설정을 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
   base: '/imoji-editor/',
-  title: 'imoji-editor'
+  title: 'imoji-editor',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Example', link: '/example/' },
+      { text: 'Options', link: '/options/' },
+      { text: 'Features', link: '/features/' }
+    ]
+  }
 };
